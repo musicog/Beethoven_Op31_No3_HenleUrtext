fb6f40e0902f96ac0a9bccb119b525fc28c16dc6
Werner Goebl <goebl@mdw.ac.at>
2025-05-30 16:34:15 +0200

    Fix file names

--- expand.sh
+++ expand.sh
@@ -1,4 +1,4 @@
-verovio --expand expansion-default -a -t mei -o Beethoven_Op31_No3_1_HenleUrtext-expanded.mei Beethoven_Op31_No3_1-HenleUrtext.mei
-verovio --expand expansion-default -a -t mei -o Beethoven_Op31_No3_2_HenleUrtext-expanded.mei Beethoven_Op31_No3_2-HenleUrtext.mei
-verovio --expand expansion-default -a -t mei -o Beethoven_Op31_No3_3_HenleUrtext-expanded.mei Beethoven_Op31_No3_3-HenleUrtext.mei
-verovio --expand expansion-default -a -t mei -o Beethoven_Op31_No3_4_HenleUrtext-expanded.mei Beethoven_Op31_No3_4-HenleUrtext.mei
+verovio --expand expansion-default -a -t mei -o Beethoven_Op31_No3_1-HenleUrtext-expanded.mei Beethoven_Op31_No3_1-HenleUrtext.mei
+verovio --expand expansion-default -a -t mei -o Beethoven_Op31_No3_2-HenleUrtext-expanded.mei Beethoven_Op31_No3_2-HenleUrtext.mei
+verovio --expand expansion-default -a -t mei -o Beethoven_Op31_No3_3-HenleUrtext-expanded.mei Beethoven_Op31_No3_3-HenleUrtext.mei
+verovio --expand expansion-default -a -t mei -o Beethoven_Op31_No3_4-HenleUrtext-expanded.mei Beethoven_Op31_No3_4-HenleUrtext.mei
